716936820342f7cd31b125f8baab802e1110d503
d3 <delsterone@gmail.com>
2025-04-05 18:28:38 -0500

    basic data types

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -1,19 +1,36 @@
 pragma language_version >= 0.14.0;
 
 import CompactStandardLibrary;
-
+export enum STATE { empty, filed}
 export ledger record_hash: Maybe<Bytes<32>>;
-export ledger doctor_id: Bytes<32>;
 export ledger patient_id  : Bytes<32>
-export ledger doctorPermissions: Maybe<Map<Bytes, String>>; // e.g., "read", "write"
+// export ledger doctorPermissions: Maybe<Map<Bytes, String>>; // e.g., "read", "write"
 
-constructor(patientId: Bytes<32>, doctor_id: Bytes<32>) {
-    patientRecordOwner = patientId;
-	doctorId = doctor_id;
-    recordHash = none<Bytes>();
-	doctorPerms = none<Map<Bytes, String>>();
+constructor() {
+    record_hash = none<Bytes>();
+	state = STATE.empty;
+	doctorPermissions = none<Map<Bytes, String>>();
 }
 
-export circuit add_record(new_record_hash : Bytes<32>): [] { 
-	
+witness patient_secret_key() : Bytes<32>;
+
+export circuit add_record(new_record_hash: Bytes): [] {
+    assert state == STATE.empty
+        "Attempted to add record to an already existing record, use update_record instead";
+	record_hash = new_record_hash; 
+    state = STATE.filed;
+}
+
+export circuit update_record(new_record_hash : Bytes) [] {
+	assert state == STATE.filled
+        "Attempted to add record to an non-existing record, use add_record instead";
+	record_hash = new_record_hash;
+
+}
+
+export circuit delete_record() [] {
+	assert state == STATE.filled
+        "Attempted to delete record a non-existing record";
+	record_hash = none<Bytess<32>>;
+	state = STATE.empty;
 }
