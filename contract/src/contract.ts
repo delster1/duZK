295763789b2c0a8c96e4ed07cdb7a7de5c731b97
d3 <delsterone@gmail.com>
2025-04-05 20:18:18 -0500

    trying to get it compiled

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -1,36 +1,42 @@
-pragma language_version >= 0.14.0;
-
 import CompactStandardLibrary;
+
 export enum STATE { empty, filed}
+
+export ledger state : STATE;
 export ledger record_hash: Maybe<Bytes<32>>;
-export ledger patient_id  : Bytes<32>
-// export ledger doctorPermissions: Maybe<Map<Bytes, String>>; // e.g., "read", "write"
+export ledger patient_id  : Bytes<32>;
 
 constructor() {
     record_hash = none<Bytes>();
-	state = STATE.empty;
-	doctorPermissions = none<Map<Bytes, String>>();
-}
+    state = STATE.empty;
+    // doctorPermissions = none<Map<Bytes, String>>(); // Removed as it's not declared as ledger
+};
 
-witness patient_secret_key() : Bytes<32>;
+witness patient_secret_key() : Bytes;
 
-export circuit add_record(new_record_hash: Bytes): [] {
+export circuit add_patient_and_record(new_record_hash: Bytes, patient_id : Bytes): [] {
     assert state == STATE.empty
         "Attempted to add record to an already existing record, use update_record instead";
-	record_hash = new_record_hash; 
+    record_hash = some<Bytes>(new_record_hash);
     state = STATE.filed;
+    patient_id = disclose(public_key(patient_secret_key(), new_record_hash));
 }
 
-export circuit update_record(new_record_hash : Bytes) [] {
-	assert state == STATE.filled
+export circuit update_record(new_record_hash : Bytes): [] {
+    assert state == STATE.filed
         "Attempted to add record to an non-existing record, use add_record instead";
-	record_hash = new_record_hash;
-
+    record_hash = some<Bytes>(new_record_hash);
 }
 
-export circuit delete_record() [] {
-	assert state == STATE.filled
+export circuit delete_record(): [] {
+    assert state == STATE.filed
         "Attempted to delete record a non-existing record";
-	record_hash = none<Bytess<32>>;
-	state = STATE.empty;
+    record_hash = none<Bytes>();
+    state = STATE.empty;
+}
+
+export circuit public_key(sk: Bytes, data: Bytes): Bytes {
+    return persistent_hash<Vector<3, Bytes>>([pad(32, "record:pk:"),
+                                                  data,
+                                                  sk]);
 }
---
+++ contract/src/witness.ts
@@ -0,0 +1,17 @@
+import { Ledger } from './managed/duZK/contract/index.cjs'
+import { WitnessContext } from '@midnight-ntwrk/compact-runtime';
+
+export type RecordPrivateState = {
+	readonly patientKey: Uint8Array;
+}
+
+export const createRecordPrivateState = (patientKey : Uint8Array) => ({
+	patientKey,
+});
+
+export const witnesses = {
+  patient_secret_key: ({ privateState }: WitnessContext<Ledger, RecordPrivateState>): [RecordPrivateState, Uint8Array] => [
+    privateState,
+    privateState.secretKey,
+  ],
+};
